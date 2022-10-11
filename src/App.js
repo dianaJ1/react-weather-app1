6bb7574c932183e1e246f663e120dc81a3524fb1
dianaJ1 <108881394+dianaJ1@users.noreply.github.com>
2022-10-11 12:50:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by Diana Jethwa
+          <a
+            href="https://github.com/dianaJ1/react-weather-app.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
